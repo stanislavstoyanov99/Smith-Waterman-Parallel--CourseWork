06855ee7cfd80f15306ae3b0edee0ecfdd9f6ed6
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-06-26 19:35:06 +0300

    Refactor code

--- Smith-Waterman-Parallel.py
+++ Smith-Waterman-Parallel.py
@@ -2,14 +2,12 @@ import threading
 import time
 import numpy as np
 from matplotlib import pyplot as plt
-#import timeit
-# from array import array
 
 # Test Case 1
-seq1 = "ATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCAT"
-seq2 = "TTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAA"
+#seq1 = "ATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCAT"
+#seq2 = "TTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAA"
 
-# Test Case 2E
+# Test Case 2
 # seq1 = "ATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCATATAGACGACATGGGGACAGCAT"
 # seq2 = "TTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAA"
 
@@ -26,10 +24,10 @@ seq2 = "TTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGCATGCGCATATCAGCAATTTAGC
 #seq2 = "TTTAGCATGCG"
 
 # Test Case 6
-#seq1 = "ATAGAC"
-#seq2 = "TTTAGC"
+seq1 = "ATAGAC"
+seq2 = "TTTAGC"
 
-match = 1
+match = 2
 mismatch = -1
 gap = -2
 maxScore = 0
@@ -44,17 +42,18 @@ score_index_list = []
 temp_list = []
 threads = []
 
-
 def score_function(row_index, col_index):
+
     global score_matrix
     global maxScore
     global maxPosition
-    # print('\nWorker: %s' % num)
+
     similarity = match if seq1[row_index - 1] == seq2[col_index - 1] else mismatch
     diag_score = score_matrix[row_index - 1][col_index - 1] + similarity
     up_score = score_matrix[row_index - 1][col_index] + gap
     left_score = score_matrix[row_index][col_index - 1] + gap
     curMax = max(0, diag_score, up_score, left_score)
+    
     if curMax > maxScore:
         maxScore = curMax
         maxPosition = (row_index, col_index)
@@ -62,6 +61,7 @@ def score_function(row_index, col_index):
     return
 
 def createScoreMatrix(rows, cols):
+
     score_matrix = [[0 for col in range(cols+1)]for row in range(rows+1)]
     return score_matrix
 
@@ -79,7 +79,6 @@ def antidiagonals_indices(rows, cols):
         q = i
 
         while(p < rows + 1 and q >= 1):
-            #print(p, q)
             temp_list.append([p, q])
             p = p + 1
             q = q - 1
@@ -88,13 +87,9 @@ def antidiagonals_indices(rows, cols):
         p = i + 1
         q = cols
         while(p < rows+1 and q >= 1):
-            #print(p, q)
             temp_list.append([p, q])
             p = p + 1
             q = q - 1
-
-    # print(temp_list)
-
 
 def traceback(score_matrix, start_pos):
 
@@ -132,73 +127,53 @@ def traceback(score_matrix, start_pos):
 
     return ''.join(reversed(aligned_seq1)), ''.join(reversed(aligned_seq2)), ''.join(reversed(aligned_seq_og1)), ''.join(reversed(aligned_seq_og2))
 
-
 def nextMove(score_matrix, x, y):
 
-    # Assign the diagonal score
     diag = score_matrix[x - 1][y - 1]
-    # print(diag)
     curr = score_matrix[x][y]
-    # Assign insertion/deletion scores
     up = score_matrix[x - 1][y]
     left = score_matrix[x][y - 1]
+
     if (curr == diag + match or curr == diag + mismatch) and curr != up + gap and curr != left + gap : return 1
     elif up > left and up>=diag: return 2
     elif left > up and left>=diag: return 3
     elif curr == 0 : return 0
-###############################################
-## Creates the alignment string for printing ##
-###############################################
-
 
 def alignment_string(aligned_seq1, aligned_seq2):
 
-    # Sets initial values
     idents, gaps, mismatches = 0, 0, 0
     alignment_string = []
 
-    # Runs through both strings
     for base1, base2 in zip(aligned_seq1, aligned_seq2):
-
-        # Checks for match
         if base1 == base2:
             alignment_string.append('| ')
             idents += 1
-
-        # Checks for insertion/deletion
         elif '-' in (base1, base2):
             alignment_string.append(' ')
             gaps += 1
-
-        # If neither of the above, it's mismatch
         else:
             alignment_string.append(': ')
             mismatches += 1
 
-    # Returns the "alignment" string and the alignment characteristics
     return ''.join(alignment_string), idents, gaps, mismatches
-
 
 execution_start = time.time()
 score_matrix = createScoreMatrix(rows, cols)
 antidiagonals_indices(rows, cols)
 
 antidiagonals = antidiagonals_list_generator(score_matrix)
-# print(antidiagonals)
 num_diag = len(antidiagonals)
 offset_counter = 0
 thread_start = time.time()
+
 for i in range(num_diag - 2 ):         
-    #print(i + 1)
     q = len(antidiagonals[i]) 
     x = 0
     threads.clear()
-    # print(q)
+
     while(x < q):
-        #print(temp_list[x + offset_counter])
         r_index = temp_list[x + offset_counter][0]
         c_index = temp_list[x + offset_counter][1]
-        #print(r_index, c_index)
         x = x + 1
 
         t = threading.Thread(target=score_function, args=(r_index, c_index))
@@ -209,33 +184,28 @@ for i in range(num_diag - 2 ):
 
     offset_counter = offset_counter + q - 1 
 
-    # print(x)
 thread_end = time.time()
+
 print(seq1)
 print(seq2)
+
 for i in score_matrix:
     print(i)
-# for y in range(len(score_matrix)):
-# print(score_matrix[y])
-'''
-for i in range(5):
-    t = threading.Thread(target=score_function, args=(i,))
-    threads.append(t)
-    t.start()
-'''
+
 print(maxPosition)
 print(maxScore)
 seq1_aligned, seq2_aligned, seq1_aligned_og, seq2_aligned_og = traceback(score_matrix, maxPosition)
 print(seq1_aligned_og)
 print(seq2_aligned_og)
+
 assert len(seq1_aligned) == len(seq2_aligned)
 
 execution_end = time.time()
+
 print("Overall time to execute: {} seconds".format(
     execution_end - execution_start))
 print("Time to run threads: {} seconds".format(thread_end - thread_start))
-# Pretty print the results. The printing follows the format of BLAST results
-# as closely as possible.
+
 alignment_str, idents, gaps, mismatches = alignment_string(
     seq1_aligned, seq2_aligned)
 
@@ -284,8 +254,6 @@ for i in range(len(seq2[start2:end2])):
         text = ax1.text(j, i, result_matrix[i][j], ha="center", va="center", color="w")
 
 pie_labels = [f'Matches({idents}/{alength})', f'Gaps({gaps}/{alength})', f'Missmatches({mismatches}/{alength})']
-
-# ax2.axis('equal')
 
 percents = [idents/alength, gaps/alength, mismatches/alength]
 
